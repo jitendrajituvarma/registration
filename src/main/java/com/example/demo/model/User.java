6a7536af9d5ac20a302d79ea5ed4520154bbbd89
jitendra kumar <jitendrakv11@gmail.com>
2022-10-17 11:55:20 +0530

    added h2 db configuration and jpa configuration

--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -1,6 +1,11 @@
 package com.example.demo.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class User {
+	@Id
   private int id;
   private String emailId;
   private String userName;
