c0c1d582d1a5680c0dc8a124d2558cf5558b5761
jitendra kumar <jitendrakv11@gmail.com>
2022-10-17 12:12:52 +0530

    add save registration methode in service.class

--- src/main/java/com/example/demo/controller/RegistrationController.java
+++ src/main/java/com/example/demo/controller/RegistrationController.java
@@ -1,8 +1,21 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.model.User;
+import com.example.demo.service.RegistrationService;
 
 @RestController
 public class RegistrationController {
 
+	@Autowired
+	RegistrationService registrationService;
+	
+	public void registerUser(@RequestBody User user) {
+		
+	}
+	
+	
 }
--- src/main/java/com/example/demo/service/RegistrationService.java
+++ src/main/java/com/example/demo/service/RegistrationService.java
@@ -1,8 +1,18 @@
 package com.example.demo.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.example.demo.model.User;
+import com.example.demo.repository.RegistrationRepo;
 
 @Service
 public class RegistrationService {
-
+  
+	@Autowired
+	RegistrationRepo registrationRepo;
+	
+	 public void saveUser(User user) {
+		 registrationRepo.save(user);
+	 }
 }
